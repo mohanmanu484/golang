cde600a482de1f2454825219c71ccac6894b17fa
mohang <mohanmanu484@gmail.com>
2019-03-24 23:38:19 +0530

    heroku

--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		//port="8000"
+		log.Fatal("$PORT must be set")
+	}
 	http.HandleFunc("/",handler)
-	http.ListenAndServe(":3000",nil)
+	http.ListenAndServe(":"+port,nil)
 }
 
 func handler(writer http.ResponseWriter, request *http.Request) {
